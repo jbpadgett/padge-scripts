4aee9623982e05598f7dbe3100729c479c27b41d
jbpadgett <jeff@jbpadgett.com>
2015-03-05 17:29:50 -0800

    adding vagrant global status destroy script

---
+++ bash/vagrant-gsd.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#debug mode
+#set -x
+set -o errexit -o nounset -o pipefail
+
+# automate killing each vagrant VM or container
+# assumes awk output of specific line counts which can be tweaked as needed
+
+vgs=$(vagrant global-status | awk 'NR==3, NR==5 {print $1}')
+for i in $vgs 
+#do echo "vagrant destroy $i"
+do yes | vagrant destroy $i
+done
+
+
+
+
+
+
+
+
