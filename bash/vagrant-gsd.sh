cc2f97086f646d11f8824ee5e295ff73ad768e87
jbpadgett <jeff@jbpadgett.com>
2015-04-06 00:05:45 -0700

    uncomplicating logic and fixing inner loop

--- bash/vagrant-gsd.sh
+++ bash/vagrant-gsd.sh
@@ -5,35 +5,24 @@
 set -o errexit -o nounset -o pipefail
 
 # automate killing each vagrant VM or container locally
-# assumes awk output of specific line counts which can be tweaked as needed
+# assumes awk output of specific line counts which can be tweaked as needed.
 
-function arrayElementcheck() {
-	myelements=${1}
-	myelement=${2}
-	for i in ${myelements[@]} ;
-	do
-		if [ $i == $myelement ] ;
-		then
-			return 1
-		fi
-	done
-	return 0
-}
 
-# I have a separate vagrant scripts directory, so I get back up one dir
 cd ..
 vgs=$(vagrant global-status | awk 'NR==3, NR==5 {print $1}')
 
-if arrayElementcheck $vgs There || you || started
-then
-	echo "All containers and host vm deleted!"
-else
-	for i in $vgs 
-	#do echo "vagrant destroy $i"
-	do yes | vagrant destroy --force $i
-	done
+for i in $vgs
+do 
+if [[ $vgs =~ "There" ]]
+	then
+		echo "==> All containers and the host vm deleted!"
+	else
+		echo "==> The following containers and VMs will be deleted now!"
+		echo $vgs
+		yes | vagrant destroy --force $i
 fi
-
+done
+echo "==> Final output of vagrant global-status..."
 vagrant global-status
 
 
