8c9e2995959dae5020af77c0b547e0f84137c062
jbpadgett <jeff@jbpadgett.com>
2015-04-17 23:02:47 -0700

    modified pg setup script

--- sql/db_setup_postgres.sh
+++ sql/db_setup_postgres.sh
@@ -2,28 +2,28 @@
 
 #debug mode
 #set -x
-set -o errexit -o nounset -o pipefail
+set -o errexit -o nounset
 
 # Generic postgres shell script execution
 # This version uses psql as an inline command with -c
 
 # Vars for connecting to postgres db and create app user role
 # Values as variables of already exported values
-DBNAME=$POSTGRES_ENV_DB || "postgres"
-DBUSER=$POSTGRES_ENV_USER || "postgres"
-DBPASSWORD=$POSTGRES_ENV_PASSWORD || "postgres"
-DBHOST=$POSTGRES_ENV_HOSTNAME
+DBNAME="$POSTGRES_ENV_DB" || "postgres"
+DBUSER="$POSTGRES_ENV_USER" || "postgres"
+DBPASSWORD="$POSTGRES_ENV_PASSWORD" || "postgres"
+DBHOST="$POSTGRES_ENV_HOSTNAME"
 DBPORT="5432"
 
 
 #Execute the sql using psql as an inline command with -c
 psql \
      -X \
-     -U $DBUSER \
-     -W $DBPASSWORD \
-     -h $DBHOST \
-     -p $DBPORT \
-     -d $DBNAME \
+     -U "$DBUSER" \
+     -W "$DBPASSWORD" \
+     -h "$DBHOST" \
+     -p "$DBPORT" \
+     -d "$DBNAME" \
      --echo-all \
      --set AUTOCOMMIT=off \
      --set ON_ERROR_STOP=on
